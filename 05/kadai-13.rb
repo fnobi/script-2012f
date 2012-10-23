60e6b6418a1d0e460227b696e9866ba89a77275c
fnobi <shin@fnobi.com>
2012-10-23 13:32:09 +0900

    runnerはファイル分けた。

--- 05/kadai-13.rb
+++ 05/kadai-13.rb
@@ -41,16 +41,3 @@ class Student
     end
   end
 end
-
-
-# 動作テスト
-# taro = Student.new('太郎')
-# taro.set_score('数学', 65)
-# taro.set_score('国語', 91)
-
-# hanako = Student.new('花子')
-# hanako.set_score('数学',80)
-# hanako.set_score('国語',77)
-
-# puts hanako.compare(taro, '数学')  # 花子の側から見て太郎と比較する
-# puts taro.compare(hanako, '国語')  # 太郎の側から見て花子と比較する
